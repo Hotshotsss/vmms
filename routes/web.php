47c882c3b275d58d862588f1e6f59c9e23db68f9
eurekathon2 <joycenpotestades@gmail.com>
2018-06-12 16:56:41 +0800

    routes

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,26 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//Add all the routes for the admin here
+Route::prefix('admin')->group(function () {
+  Route::get('/',function(){
+
+  });
+});
+//Add all the routes for the montiring here
+Route::prefix('monitoring')->group(function () {
+  Route::get('/',function(){
+
+  });
+});
+
+//Add all the routes for the gate here
+Route::prefix('gate')->group(function () {
+  Route::get('/',function(){
+
+  });
+});
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,7 +40,6 @@ Route::get('test','GateController@indexGate')->middleware('auth');
 
 Route::get('test2','GateController@Gate2');
 
-///
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
