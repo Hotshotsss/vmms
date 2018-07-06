509c70605568862f5aaf8a3192b2b43df6c4c5a4
Joyce <joycenpotestades@gmail.com>
2018-07-06 22:03:50 +0800

    yey

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -8,10 +8,12 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>{{ config('app.name', 'Laravel') }}</title>
+  <title>VMMS</title>
 
   <!-- Styles -->
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <link rel="stylesheet" href="{{asset('css/sample.css')}}">
+
 </head>
 <body>
 
@@ -19,7 +21,7 @@
     <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
       <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
-          {{ config('app.name', 'Laravel') }}
+          VMMS
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -35,7 +37,7 @@
           <ul class="navbar-nav ml-auto">
             <!-- Authentication Links -->
             @guest
-        
+
             @else
             <li class="nav-item dropdown">
               <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
