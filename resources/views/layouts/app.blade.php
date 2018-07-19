62684228f58984c9be45ae210dc5d0b1ec2720d7
unknown <joycenpotestades@gmail.com>
2018-07-19 17:18:08 +0800

    almowst

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,18 +14,18 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
     <!-- MetisMenu CSS -->
-    <link href="{{ url ('vendor/metisMenu/metisMenu.min.css')}}" rel="stylesheet">
+    <link href="{{ asset ('css/metisMenu.min.css')}}" rel="stylesheet">
 
     <!-- Custom CSS -->
 
-    <link href="{{ url ('dist/css/sb-admin-2.css')}}" rel="stylesheet">
-    <link href="{{ url ('css/style.css')}}" rel="stylesheet">
+    <link href="{{ asset ('css/sb-admin-2.css')}}" rel="stylesheet">
+    <link href="{{ asset ('css/style.css')}}" rel="stylesheet">
 
     <!-- Morris Charts CSS -->
-    <link href="{{ url ('vendor/morrisjs/morris.css')}}" rel="stylesheet">
+    <link href="{{ asset ('css/morris.css')}}" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="{{ url ('vendor/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{ asset ('css/fontawesome.min.css')}}" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -145,16 +145,10 @@
 </body>
 </html>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
+<script src="{{asset('js/jquery.min.js')}}"></script>
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
 <!-- Metis Menu Plugin JavaScript -->
-<script src="{{ url ('vendor/metisMenu/metisMenu.min.js')}}"></script>
-
-<!-- Morris Charts JavaScript -->
-<script src="{{ url ('vendor/raphael/raphael.min.js')}}"></script>
-<script src="{{ url ('vendor/morrisjs/morris.min.js')}}"></script>
-<script src="{{ url ('data/morris-data.js')}}"></script>
+<script src="{{ asset ('js/metisMenu.min.js')}}"></script>
 
 <!-- Custom Theme JavaScript -->
-<script src="{{ url ('dist/js/sb-admin-2.js')}}"></script>
+<script src="{{ asset ('js/sb-admin-2.js')}}"></script>
--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -14,52 +14,50 @@
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <link rel="stylesheet" href="{{asset('css/sample.css')}}">
   <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+  <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
   <!-- Optional theme -->
 
   <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="{{asset('css/admin.css')}}">
 
 </head>
 <style media="screen">
-  .container{
-    position:absolute;bottom:70px;left:0;right:0;
-  }
+.container{
+  position:absolute;bottom:70px;left:0;right:0;
+}
+.logoPng{
+  width: 50%;
+  margin-bottom: 0px;
+}
+@media(max-width:768px){
   .logoPng{
-    width: 50%;
+    width: 80%;
     margin-bottom: 0px;
   }
-  @media(max-width:768px){
-    .logoPng{
-      width: 80%;
-  margin-bottom: 0px;
-    }
+}
+@media(max-width:1024px){
+  .container{
+    top:70px;bottom:0;left:0;right:0;
   }
-  @media(max-width:1024px){
-    .container{
-      top:70px;bottom:0;left:0;right:0;
-    }
+}
+@media(max-width:767px){
+  body{
+    background: white;
   }
-  @media(max-width:767px){
-    body{
-      background: white;
-    }
-    .container{
-      top:20px;
-    }
+  .container{
+    top:20px;
   }
+}
 </style>
 <body>
 
   <div id="app">
 
 
-          <main class="py-4">
-            @yield('content')
-          </main>
-        </div>
-      </body>
-      <script src="{{ asset('js/app.js') }}"></script>
-      </html>
+    <main class="py-4">
+      @yield('content')
+    </main>
+  </div>
+</body>
+<script src="{{ asset('js/app.js') }}"></script>
+</html>
